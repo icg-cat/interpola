39afd4e7016e3eebadd3d88cce20a7bfc7ab4ccd
irene_laptop <irene.cruz@uab.cat>
2024-11-22 07:23:40 +0100

    update citació

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@ current_version <- packageVersion("interpola")
 
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(
-    "Gràcies per fer servir aquest paquet. L'heu de referenciar com segueix:\n\n",
+    "Gràcies per fer servir aquest paquet, que s'ha desenvolupat dins de les línies de treball de l'Institut Metròpoli (institutmetropoli.cat). Si us plau, referencieu-lo com segueix:\n\n",
     "Cruz-Gómez, Irene (2024). interpola. Eines per la interpolació de dades espacials. ",
     "Versió ", current_version[[1]],
     ". https://github.com/icg-cat/interpola")
